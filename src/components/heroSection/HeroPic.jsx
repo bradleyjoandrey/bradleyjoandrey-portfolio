cdc147472d2ce47fb390d4ff7ca913f6279ff896
Bradley <bradleyjoandrey.sg@gmail.com>
2025-07-01 01:06:02 +0800

    Fix image path for GitHub Pages

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -4,7 +4,7 @@ import { PiCircleThin } from "react-icons/pi";
 const HeroPic = () => {
     return (
         <div className='h-full flex items-center justify-center'>
-            <img src='../public/images/HeroImage.png' alt='Bradley Joandrey' className='max-h-[500px] w-auto'></img>
+            <img src={`${import.meta.env.BASE_URL}images/HeroImage.png`} alt='Bradley Joandrey' className='max-h-[500px] w-auto'></img>
             <div className='absolute -z-10 flex justify-center items-center animate-pulse'>
                 <PiCircleThin className='md:h-[90%] sm:h-[120%] min-h-[500px] w-auto text-gray-700 blur-md animate-[pulse_6s_ease-in-out_infinite]'/> 
             </div>
